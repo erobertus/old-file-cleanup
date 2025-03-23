0bbc0462c9c98abe6c36904bfb61da6e902e9f2d
erobertus <robertus@rogers.com>
2025-03-23 14:32:24 -0400

    Update uninstall script to remove logrotate config for cleanup.sh

--- uninstall_cleanup.sh
+++ uninstall_cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Define the script name and its destination directory
 SCRIPT_NAME="cleanup.sh"
 DESTINATION="/usr/local/bin/$SCRIPT_NAME"
+LOGROTATE_CONF="/etc/logrotate.d/cleanup_log"
 
 # Remove the script from /usr/local/bin/
 if [ -f "$DESTINATION" ]; then
@@ -25,3 +25,16 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Cron job for $SCRIPT_NAME has been successfully removed."
+
+# Remove logrotate config if it exists
+if [ -f "$LOGROTATE_CONF" ]; then
+  sudo rm "$LOGROTATE_CONF"
+  if [ $? -ne 0 ]; then
+    echo "Failed to remove logrotate configuration at $LOGROTATE_CONF."
+    exit 1
+  else
+    echo "Logrotate configuration $LOGROTATE_CONF has been removed."
+  fi
+else
+  echo "No logrotate configuration found at $LOGROTATE_CONF."
+fi
